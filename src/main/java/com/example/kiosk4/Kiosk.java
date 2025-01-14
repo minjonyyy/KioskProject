171f12490efd9c4c193745677dc3ecffb8075f4e
minjonyyy <minjonyyy@naver.com>
2025-01-14 23:35:50 +0900

    Refactor : 사용자 입력 예외처리 try-catch문으로 변경

--- src/main/java/com/example/kiosk4/Kiosk.java
+++ src/main/java/com/example/kiosk4/Kiosk.java
@@ -33,36 +33,42 @@ public class Kiosk {
             if (selectCategoryNum==0) {
                 System.out.println("프로그램을 종료합니다.");
                 break;
-            } else if (selectCategoryNum>=1 && selectCategoryNum<=menus.size()){
-                Menu selectCategory = menus.get(selectCategoryNum-1);
-
-                while(true){
-                    System.out.println("-----------------------");
-                    System.out.println("[ " + selectCategory.getMenuCategory() + " MENU ]");
-                    selectCategory.printMenuItems();
-                    System.out.println("0. 뒤로가기");
-
-                    //-------------- 사용자 선택2 (메뉴 아이템 선택) -------------------------
-                    System.out.print("메뉴를 골라주세요. : ");
-                    int selectMenuItemNum = sc.nextInt();
-
-                    if (selectMenuItemNum==0) {
-                        break; //뒤로가기
-                    }
-
-                    MenuItem selectMenuItem = selectCategory.getMenuItems().get(selectMenuItemNum-1);
-
-                    if (selectMenuItemNum>=1 && selectMenuItemNum <= selectCategory.getMenuItems().size()){
-                        System.out.println("선택한 메뉴: < " + selectMenuItem.getName() + " | " + "W " + selectMenuItem.getPrice() + " | " + selectMenuItem.getInfo() + " >");
-
-                    } else {
-                        System.out.println("잘못된 숫자입니다. 다시 입력해주세요."); //이거 왜 출력 안되고 오류뜸??;;
-                    }
-                }
-            } else {
-                System.out.println("잘못된 숫자입니다. 다시 입력해주세요.");
             }
 
+            try {
+                Menu selectCategory = menus.get(selectCategoryNum-1);
+
+                if (selectCategoryNum>=1 && selectCategoryNum<=menus.size()){
+                    while(true){
+                        System.out.println("-----------------------");
+                        System.out.println("[ " + selectCategory.getMenuCategory() + " MENU ]");
+                        selectCategory.printMenuItems();
+                        System.out.println("0. 뒤로가기");
+
+                        //-------------- 사용자 선택2 (메뉴 아이템 선택) -------------------------
+                        System.out.print("메뉴를 골라주세요. : ");
+                        int selectMenuItemNum = sc.nextInt();
+
+                        if (selectMenuItemNum==0) {
+                            break; //뒤로가기
+                        }
+
+                        try {
+                            MenuItem selectMenuItem = selectCategory.getMenuItems().get(selectMenuItemNum-1);
+
+                            if (selectMenuItemNum>=1 && selectMenuItemNum <= selectCategory.getMenuItems().size()){
+                                System.out.println("선택한 메뉴: < " + selectMenuItem.getName() + " | " + "W " + selectMenuItem.getPrice() + " | " + selectMenuItem.getInfo() + " >");
+
+                            }
+                        } catch (IndexOutOfBoundsException e){
+                            System.out.println("잘못된 숫자입니다. 다시 입력해주세요.");
+                        }
+
+                    }
+                }
+            } catch (IndexOutOfBoundsException e){
+                System.out.println("잘못된 숫자입니다. 다시 입력해주세요.");
+            }
 
 
         }
