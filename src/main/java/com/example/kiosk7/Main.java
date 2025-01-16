80edaf1102da5d0f7eedae035b853f6a62a38380
minjonyyy <minjonyyy@naver.com>
2025-01-16 15:35:49 +0900

    init : kiosk7 프로젝트 초기 생성 (도전기능 Lv.2)

---
+++ src/main/java/com/example/kiosk7/Main.java
@@ -0,0 +1,34 @@
+package com.example.kiosk7;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Main {
+    public static void main(String[] args) {
+        // 버거 객체 생성
+        Menu Burgers = new Menu("BURGERS");
+
+        Burgers.addMenuItems(new MenuItem("ShackBurger", 6.9, "토마토, 양상추, 쉑소스가 토핑된 치즈버거"));
+        Burgers.addMenuItems(new MenuItem("SmokeShack", 8.9, "베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거"));
+        Burgers.addMenuItems(new MenuItem("Cheeseburger", 6.9, "포테이토 번과 비프패티, 치즈가 토핑된 치즈버거"));
+        Burgers.addMenuItems(new MenuItem("Hamburger", 5.4, "비프패티를 기반으로 야채가 들어간 기본버거"));
+
+        // 음료, 디저트 객체 생성
+        Menu Drinks = new Menu("DRINKS");
+        Menu Desserts = new Menu("DESSERTS");
+
+        // 메뉴판 리스트 생성
+        List<Menu> menus = new ArrayList<>();
+        menus.add(Burgers);
+        menus.add(Drinks);
+        menus.add(Desserts);
+
+
+        Cart cart = new Cart();
+
+        /// 키오스크 객체 생성 및 시작하는 함수 호출
+        Kiosk kiosk = new Kiosk(menus, cart);
+        kiosk.start();
+    }
+}
---
+++ src/main/java/com/example/kiosk7/Menu.java
@@ -0,0 +1,42 @@
+package com.example.kiosk7;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Menu {
+    // Menu 클래스의 필드
+    private final String menuCategory;
+    private final List<MenuItem> menuItems;
+
+    // 생성자
+    public Menu(String menuCategory){
+        this.menuCategory = menuCategory;
+        this.menuItems = new ArrayList<>();
+    }
+
+    //메뉴 카테고리를 반환하는 getter 메서드
+    public String getMenuCategory(){
+        return menuCategory;
+    }
+
+    // 메뉴 아이템 추가하는 메서드
+    public void addMenuItems(MenuItem item){
+        menuItems.add(item);
+    }
+
+    public void printMenuItems() {
+        for(int i=0; i<menuItems.size(); i++){
+            System.out.print((i+1) + ". " + menuItems.get(i).getName());
+            System.out.print("  | W " + menuItems.get(i).getPrice());
+            System.out.println("| " + menuItems.get(i).getInfo());
+        }
+    }
+
+    public List<MenuItem> getMenuItems(){
+        return menuItems;
+    }
+
+
+
+
+}
