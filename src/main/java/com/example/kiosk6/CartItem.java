435f12382cd7b8e12594c0d352be4ac8764035ca
minjonyyy <minjonyyy@naver.com>
2025-01-16 14:45:18 +0900

    Feat : CartItem 클래스 추가

---
+++ src/main/java/com/example/kiosk6/CartItem.java
@@ -0,0 +1,33 @@
+package com.example.kiosk6;
+
+public class CartItem {
+
+    private final String name;
+    private final double price;
+    private int quantity;
+
+
+    public CartItem(String name, double price, int quantity) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+
+    public double getPrice() {
+        return price;
+    }
+
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity){
+        this.quantity = quantity;
+    }
+}
