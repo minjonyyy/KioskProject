7361736ef4827ebaca94d6c983dbce19f04b719a
minjonyyy <minjonyyy@naver.com>
2025-01-17 14:10:50 +0900

    feat : enum 타입 속성 추가, 할인계산해주는 메서드 추가

--- src/main/java/com/example/kiosk7/Discount.java
+++ src/main/java/com/example/kiosk7/Discount.java
@@ -4,17 +4,19 @@ public class Discount {
 
 
     public enum DiscountType {
-        VETERAN("국가유공자", 10),
-        SOLDIER("군인",5),
-        STUDENT("학생",3),
-        GENERAL("일반",0);
+        VETERAN(1,"국가유공자", 10),
+        SOLDIER(2,"군인",5),
+        STUDENT(3,"학생",3),
+        GENERAL(4,"일반",0);
 
         private final int discountRate;
         private final String type;
+        private final int num;
 
-        DiscountType(String type, int discountRate) {
-            this.type = type;
+        DiscountType(int num, String type, int discountRate) {
+            this.num = num;
             this.discountRate = discountRate;
+            this.type = type;
         }
 
         public int getDiscountRate(){ return discountRate; }
@@ -22,7 +24,16 @@ public class Discount {
 
     public void printDiscountRate(){
         for (DiscountType type : DiscountType.values()){
-            System.out.println("."+type.type +"  : "+ type.discountRate + "%");
+            System.out.println(type.num + "."+type.type +"  : "+ type.discountRate + "%");
         }
+    }
+
+    public double calculateDiscount(int discountTypeNum, double totalPrice){
+        for (DiscountType type : DiscountType.values()){
+            if ( discountTypeNum == type.num){
+                totalPrice = totalPrice * (1.0 - type.discountRate * 0.01);
+            }
+        }
+        return totalPrice;
     }
 }
