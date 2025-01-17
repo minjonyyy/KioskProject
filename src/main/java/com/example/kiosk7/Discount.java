9f121f0d6016099c2ab2c47d54f2079d39181475
minjonyyy <minjonyyy@naver.com>
2025-01-17 13:40:13 +0900

    feat : 할인율 출력 메서드 생성

--- src/main/java/com/example/kiosk7/Discount.java
+++ src/main/java/com/example/kiosk7/Discount.java
@@ -2,15 +2,27 @@ package com.example.kiosk7;
 
 public class Discount {
 
+
     public enum DiscountType {
-        VETERAN(10), SOLDIER(5), STUDENT(3), GENERAL(0);
+        VETERAN("국가유공자", 10),
+        SOLDIER("군인",5),
+        STUDENT("학생",3),
+        GENERAL("일반",0);
 
         private final int discountRate;
+        private final String type;
 
-        DiscountType(int discountRate) {
+        DiscountType(String type, int discountRate) {
+            this.type = type;
             this.discountRate = discountRate;
         }
 
         public int getDiscountRate(){ return discountRate; }
+    }
+
+    public void printDiscountRate(){
+        for (DiscountType type : DiscountType.values()){
+            System.out.println("."+type.type +"  : "+ type.discountRate + "%");
+        }
     }
 }
